f9f22571330220c0dee6bec4e075474a9df5ffa1
Andreea-Ioana Mardale <104494787+andreeamardale@users.noreply.github.com>
2023-03-09 19:45:38 +0200

    first commit

---
+++ src/ro/ase/acs/cts/classes/Writer.java
@@ -0,0 +1,4 @@
+package ro.ase.acs.cts.classes;
+
+public class Writer {
+}
---
+++ src/ro/ase/acs/cts/main/Main.java
@@ -0,0 +1,8 @@
+package ro.ase.acs.cts.main;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
