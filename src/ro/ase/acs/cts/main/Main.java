f5606a9f9cb815d127ea526a31fc28b73d5fc143
Andreea-Ioana Mardale <104494787+andreeamardale@users.noreply.github.com>
2023-03-09 21:48:35 +0200

    Last commit

--- src/ro/ase/acs/cts/main/Main.java
+++ src/ro/ase/acs/cts/main/Main.java
@@ -1,9 +1,41 @@
 package ro.ase.acs.cts.main;
 
 
+import ro.ase.acs.cts.classes.ConnectionManager;
+import ro.ase.acs.cts.classes.DataInserter;
+import ro.ase.acs.cts.classes.DataReader;
+import ro.ase.acs.cts.classes.TableCreator;
+import ro.ase.acs.cts.interfaces.IConnectionManager;
+import ro.ase.acs.cts.interfaces.IDataInserter;
+import ro.ase.acs.cts.interfaces.IDataReader;
+import ro.ase.acs.cts.interfaces.ITableCreator;
+
+import java.sql.Connection;
+
 public class Main {
 
     public static void main(String[] args) {
+
+
+        try {
+
+            IConnectionManager connectionManager = new ConnectionManager();
+            Connection connection = connectionManager.getConnection();
+
+            ITableCreator employeeTableCreator = new TableCreator();
+            employeeTableCreator.createTable(connection);
+
+            IDataInserter employeeDataInsertor = new DataInserter();
+            employeeDataInsertor.insertData(connection);
+
+            IDataReader employeeDataReader = new DataReader();
+            employeeDataReader.readData(connection);
+
+            connection.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
 
     }
